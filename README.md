0437cc073d78354a9aed53f3b3197698ea440dfb
adama-traore-github <wwwnostra7@gmail.com>
2024-06-21 00:57:41 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+TRAORE ADama
---
+++ lib/00_journalists.rb
@@ -0,0 +1,110 @@
+
+
+handles= ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+# Définition de l'array de handles
+
+#je vais mettre "puts puts" pour l'espace entre les reponses
+
+# Question 1 : Combien y a-t-il de journalistes dans ce array?
+puts "Il y a #{handles.size} journalistes dans ce array."
+puts
+puts
+# Question 2 : Combien d'handle contiennent un numéro?
+num_handles = handles.select { |handle| handle =~ /\d/ }.size
+puts "Il y a #{num_handles} handles qui contiennent un numéro."
+puts
+puts
+# Question 3 : Combien d'handle contiennent les 4 lettres du prénom "aude" à la suite?
+aude_handles = handles.select { |handle| handle.downcase.include?("aude") }.size
+puts "Il y a #{aude_handles} handles qui contiennent les 4 lettres du prénom 'aude' à la suite."
+puts
+puts
+# Question 4 : Combien commencent par une majuscule (première lettre juste après le @)?
+uppercase_handles = handles.select { |handle| handle[1].upcase == handle[1] }.size
+puts "Il y a #{uppercase_handles} handles qui commencent par une majuscule."
+puts
+puts
+# Question 5 : Combien contiennent au moins une majuscule?
+any_uppercase_handles = handles.select { |handle| handle =~ /[A-Z]/ }.size
+puts "Il y a #{any_uppercase_handles} handles qui contiennent au moins une majuscule."
+puts
+puts
+# Question 6 : Combien y a-t-il de underscore _ dans tous les pseudos confondus?
+underscore_count = handles.join.count("_")
+puts "Il y a #{underscore_count} underscore _ dans tous les pseudos confondus."
+puts
+puts
+# Question 7 : Trie la liste de handle par ordre alphabétique.
+sorted_handles = handles.sort
+puts "La liste de handle triée par ordre alphabétique est : #{sorted_handles.join(", ")}"
+
+puts
+puts
+# Question 8 : Quels sont les 50 handles les plus courts de ce array?
+short_handles = handles.sort_by { |handle| handle.size }.first(50)
+puts "Les 50 handles les plus courts sont : #{short_handles.join(", ")}"
+puts
+puts
+# Question 9 : Quelle est la position dans l'array de la personne @epenser?
+epenser_index = handles.index("@epenser")
+puts "La position de @epenser dans l'array est : #{epenser_index}"
+puts
+puts
+
+
+
+puts "1. Combien d'handles contiennent un tiret -?"
+
+puts "2. Combien d'handles contiennent un point.?"
+
+puts "3. Quel est le handle le plus long de ce array?"
+
+puts
+
+
+choice = gets.chomp.to_i
+
+
+case choice
+
+when 1
+
+  # Combien d'handles contiennent un tiret -?
+
+  dash_handles = handles.select { |handle| handle.include?("-") }.size
+
+  puts "Il y a #{dash_handles} handles qui contiennent un tiret -."
+
+  puts
+
+  puts
+
+when 2
+
+  # Combien d'handles contiennent un point.?
+
+  dot_handles = handles.select { |handle| handle.include?(".") }.size
+
+  puts "Il y a #{dot_handles} handles qui contiennent un point."
+
+  puts
+
+  puts
+
+when 3
+
+  # Quel est le handle le plus long de ce array?
+
+  longest_handle = handles.max_by { |handle| handle.size }
+
+  puts "Le handle le plus long est : #{longest_handle}"
+
+  puts
+
+  puts
+
+else
+
+  puts "Erreur, choix invalide."
+
+end
